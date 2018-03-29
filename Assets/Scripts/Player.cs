d9593656eb5e0eb6674cb37abeda8524fa8596fa
Gary <garytmulhall@gmail.com>
2018-03-29 19:53:21 +0100

    Attack!
    -Added attack functionality for the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,19 +16,78 @@ public class Player : MonoBehaviour
     public int maxHealth;
     /*An integer to keep track of the players current health value*/
     int currentHealth;
+    /*Creating a reference to the sword the player will use*/
+    public GameObject sword;
+    /*The start method is used to initialise everything required at the START of the game (mind = blown)*/
     void Start()
     {
         /*This is how we get the reference to the Animator*/
         animator = GetComponent<Animator>();
-    }
+        /*Setting the players current health to equal the maximum at the start of the game*/
+        currentHealth = maxHealth;
+        /*Calling GetHealth in case the players health is changed in the inspector*/
+        GetHealth();
 
+    }
+    /*A method to get the players current health and draw the correct amount of hearts to the screen*/
+    void GetHealth()
+    {
+        /*Looping through and disabling all of the heart sprites*/
+        for (int i = 0; i <= hearts.Length - 1; i++)
+        {
+            hearts[i].gameObject.SetActive(false);
+        }
+        /*Looping through and drawing the correct amount of hearts based on the players health*/
+        for (int i = 0; i <= currentHealth - 1; i++)
+        {
+            hearts[i].gameObject.SetActive(true);
+        }
+    }
     void Update()
     {
         /*Calling the Movement method in Update ensures it will be called every frame
          * meaning the player can move consistently based on user input*/
         Movement();
+        GetHealth();
+        /*Ensuring the players health can never go above the max health value by
+         * checking if it has and if so, setting it to equal max health*/
+         if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+         if (Input.GetKey(KeyCode.Space))
+        {
+            Attack();
+        }
     }
-
+    /*A method which contains the code for the players attacks*/
+    void Attack()
+    {
+        /*Creating a new GameObject when the player attacks by instantiating the sword,
+         * setting its position and then setting its rotation*/
+        GameObject newSword = Instantiate(sword, transform.position, sword.transform.rotation);
+        /*swordDirection is a temporary variable only available within the Attack method and is used
+         * to handle the rotation of the sword when it is spawned in the game.*/
+        int swordDirection = animator.GetInteger("Direction");
+        /*Using the same Direction integer I created previously for the players movement animations*/
+        if (swordDirection == 0) /* 0 = Up*/
+        {
+            newSword.transform.Rotate(0, 0, 0);
+        }
+        else if (swordDirection == 1) /* 1 = Down */
+        {
+            newSword.transform.Rotate(0, 0, 180);
+        }
+        else if (swordDirection == 2) /* 2 = Left */
+        {
+            newSword.transform.Rotate(0, 0, 90);
+        }
+        else if (swordDirection == 3) /* 3 = Right */
+        {
+            newSword.transform.Rotate(0, 0, -90);
+        }
+    }
+    /*A method which contains the code required for the players movement*/
     void Movement()
     {
         /*Checking for keypresses and moving the player*/
